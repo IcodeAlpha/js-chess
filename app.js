eb6447dcf839152a68c409f1c8f1995b75193b69
IcodeAlpha <alphacode4@outlook.com>
2024-09-17 16:00:59 +0300

    createboard

--- app.js
+++ app.js
@@ -15,10 +15,26 @@ const startPieces = [
 ]
 
 function createBoard() {
-    startPieces.forEach((startPiece) => {
+    startPieces.forEach((startPiece, i) => {
         const square = document.createElement('div')
         square.classList.add('square')
-        square.classList.add('beige')
+        square.innerHTML = startPiece
+        square.firstChild?.setAttribute('draggable', true)
+        square.setAttribute('square-id', i)
+        const row = Math.floor(63 - i / 8) + 1
+        if (row % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? "beige" : "brown")
+        } else {
+            square.classList.add(i % 2 === 0 ? "brown" : "beige")
+        }
+
+        if ( i <= 15) {
+            square.firstChild.firstChild.classList.add('black')
+        }
+
+        if ( i >= 48) {
+            square.firstChild.firstChild.classList.add('white')
+        }
         gameBoard.append(square)
 
     })
